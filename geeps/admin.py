ce5553fe55eb26d08afafb116f8f0a53e10e142b
Canavaggio Lorenzo <lorenzo.canavaggio@laposte.net>
2018-06-14 00:01:48 +0200

    All requested features are done

--- geeps/admin.py
+++ geeps/admin.py
@@ -5,6 +5,7 @@ from .models import Version
 from .models import Demande
 from django.http import HttpResponse
 import csv
+import datetime
 
 admin.site.register(Logiciel)
 admin.site.register(Version)
@@ -18,10 +19,11 @@ class ExportCsvMixin:
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
         writer = csv.writer(response)
-        writer.writerow(field_names)
+        writer.writerow(["ActivationLabel", "MatlabRelease", "HostID", "Name", "Firstname", "Email", "Bureau", "Telephone", "Batiment", "OperatingSystem"])
 
         for obj in queryset:
-            row = writer.writerow([getattr(obj, field) for field in field_names])
+            print(obj)
+            row = writer.writerow([getattr(obj, "prenom")+"-"+getattr(obj, "nom")+"-"+getattr(obj, "version").nom+"-"+convertDatetimeToString(getattr(obj, "created_date")),getattr(obj, "version").nom,getattr(obj, "host"),getattr(obj, "nom"),getattr(obj, "prenom"),getattr(obj, "email"),getattr(obj, "bureau"),getattr(obj, "phone"),getattr(obj, "batiment").nom,getattr(obj, "os")])
         return response
 
     export_as_csv.short_description = "Export Selected"
@@ -34,3 +36,13 @@ class DemandeAdmin(admin.ModelAdmin, ExportCsvMixin):
 	actions = ["export_as_csv"]
 
 
+def convertDatetimeToString(o):
+    DATE_FORMAT = "%Y-%m-%d" 
+    TIME_FORMAT = "%H:%M:%S"
+
+    if isinstance(o, datetime.date):
+        return o.strftime(DATE_FORMAT)
+    elif isinstance(o, datetime.time):
+        return o.strftime(TIME_FORMAT)
+    elif isinstance(o, datetime.datetime):
+        return o.strftime("%s %s" % (DATE_FORMAT, TIME_FORMAT))
